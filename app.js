677453fa47f6908d5c5533d8a2948a6fdba3df09
bluesauce838 <94112413+bluesauce838@users.noreply.github.com>
2022-11-14 20:45:54 +0530

    3 files added

---
+++ app.js
@@ -0,0 +1,7 @@
+// const http = require('http');
+// console.log(http);
+
+// const a = 10
+
+const user = require('./user');
+console.log(user.getName());
---
+++ user.js
@@ -0,0 +1,13 @@
+const getName = () => {
+  return 'John Doe'
+}
+
+const getLocation = () => {
+  return 'Munich'
+}
+
+const dateOfBirth = '12.01.1982'
+
+exports.getName = getName
+exports.getLocation = getLocation
+exports.dob = dateOfBirth
